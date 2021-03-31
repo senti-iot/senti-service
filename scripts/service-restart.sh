55fba5c972fb1c1b162af647bc8f5665a95af0c1
Andrei <at@senti.dk>
2021-03-31 19:51:19 +0200

    Gateway Slack Blocks

--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -5,7 +5,36 @@ if [[ "$1" == "master" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/gateway/production
 	systemctl restart senti-gateway.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Gateway MASTER updated and restarted!"}' $2
+	curl -X POST -H 'Content-type: application/json' --data '{
+	"blocks": [
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Senti Gateway updated",
+				"emoji": true
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*Branch:*\n'$1'"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Travis",
+					"emoji": true
+				},
+				"value": "travis-link",
+				"url": "http://travis-ci.com/github/senti-iot/senti-gateway",
+				"action_id": "button-action"
+			}
+		}
+	]
+}' $2
 	echo
 	exit 0
 fi
@@ -14,7 +43,36 @@ if [[ "$1" == "dev" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/gateway/development
 	systemctl restart senti-gateway-dev.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Gateway DEV updated and restarted!"}' $2
+	curl -X POST -H 'Content-type: application/json' --data '{
+	"blocks": [
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Senti Gateway updated",
+				"emoji": true
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*Branch:*\n'$1'"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Travis",
+					"emoji": true
+				},
+				"value": "travis-link",
+				"url": "http://travis-ci.com/github/senti-iot/senti-gateway",
+				"action_id": "button-action"
+			}
+		}
+	]
+}' $2
 	echo
 	exit 0
 fi
