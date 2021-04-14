176d609e4add12886c7ad98d5e0c405c9e2d27e9
Andrei <at@senti.dk>
2021-04-14 18:34:24 +0200

    fix service deploy

--- scripts/service-deploy.sh
+++ scripts/service-deploy.sh
@@ -3,7 +3,7 @@
 if [[ "$1" == "master" ]]; then
 	echo
 	echo Deploying Senti template $1 ...
-	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/service/production
+	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/template/production
 	echo
 	echo Restarting Senti template service: $1 ...
 	ssh deploy@rey.webhouse.net "sudo /srv/nodejs/senti/services/template/production/scripts/service-restart.sh master $3"
@@ -15,7 +15,7 @@ fi
 if [[ "$1" == "dev" ]]; then
 	echo
 	echo Deploying Senti template $1 ...
-	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/service/development
+	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/template/development
 	echo
 	echo Restarting Senti template service: $1 ...
 	ssh deploy@rey.webhouse.net "sudo /srv/nodejs/senti/services/template/development/scripts/service-restart.sh dev $3"
